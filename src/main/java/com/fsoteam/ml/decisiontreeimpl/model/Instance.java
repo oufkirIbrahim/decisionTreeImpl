0ab627972015fc609c48c1c95401dba9b76382ac
codeonym <bouarourayoub0@gmail.com>
2024-05-30 23:43:04 +0100

    Edit[1]: Starting implementation

--- src/main/java/com/fsoteam/ml/decisiontreeimpl/model/Instance.java
+++ src/main/java/com/fsoteam/ml/decisiontreeimpl/model/Instance.java
@@ -1,4 +1,58 @@
 package com.fsoteam.ml.decisiontreeimpl.model;
 
+import java.util.List;
+import com.fsoteam.ml.decisiontreeimpl.decisionTree.Attribute;
+import com.fsoteam.ml.decisiontreeimpl.decisionTree.Branch;
+
 public class Instance {
+
+    private int instanceId;
+    private List<String> attributeValues;
+    private String classLabel;
+
+    public Instance(int instanceId, List<String> attributeValues, String classLabel) {
+        this.instanceId = instanceId;
+        this.attributeValues = attributeValues;
+        this.classLabel = classLabel;
+    }
+
+    public Instance(int instanceId, List<String> attributeValues) {
+        this.instanceId = instanceId;
+        this.attributeValues = attributeValues;
+    }
+
+    public int getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(int instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public List<String> getAttributeValues() {
+        return attributeValues;
+    }
+
+    public void setAttributeValues(List<String> attributeValues) {
+        this.attributeValues = attributeValues;
+    }
+
+    public String getValeurSeulAttribut(Attribute att) {
+        for (String attInstance : attributeValues) {
+            for (Branch b : att.getBranches()) {
+                if (attInstance.equals(b.getValue())) {
+                    return attInstance;
+                }
+            }
+        }
+        return "";
+    }
+
+    public String getClassLabel() {
+        return classLabel;
+    }
+
+    public void setClassLabel(String classLabel) {
+        this.classLabel = classLabel;
+    }
 }
