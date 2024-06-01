c466db6bfab2eb841655d8a35a0f94d2508f6236
oufkirIbrahim <oufki.bar@gmail.com>
2024-06-01 21:57:12 +0100

    edit[7] added upload data FXML and associated controller

--- src/main/java/com/fsoteam/ml/decisiontreeimpl/ui/RunTestController.java
+++ src/main/java/com/fsoteam/ml/decisiontreeimpl/ui/RunTestController.java
@@ -1,4 +1,15 @@
 package com.fsoteam.ml.decisiontreeimpl.ui;
 
+import com.fsoteam.ml.decisiontreeimpl.decisionTree.Attribute;
+import com.fsoteam.ml.decisiontreeimpl.model.Instance;
+import javafx.collections.ObservableList;
+
+import java.util.List;
+
 public class RunTestController {
+    public void setAttributes(List<Attribute> attributes) {
+    }
+
+    public void setInstanceData(ObservableList<Instance> instanceData) {
+    }
 }
